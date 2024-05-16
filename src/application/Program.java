7b17bec1b9f87c6fafb567bcefb0c6fcf68a9723
Carlos Eduardo <carloseduscc@gmail.com>
2024-05-16 15:06:48 -0300

    SellerDAOJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,8 @@ package application;
 
 import java.util.Date;
 
-import mode.entities.Seller;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
--- src/mode/dao/DepartmentDao.java
+++
@@ -1,5 +0,0 @@
-package mode.dao;
-
-public interface DepartmentDao {
-
-}
